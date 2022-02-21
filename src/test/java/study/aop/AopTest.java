50810ce4a125a971313297ad4a15a15e52ed2e01
leeyoungchun <dldydcns123@gmail.com>
2022-02-21 21:19:55 +0900

    어드바이스 순서 변경하기

--- src/test/java/study/aop/AopTest.java
+++ src/test/java/study/aop/AopTest.java
@@ -9,17 +9,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import study.aop.order.OrderRepository;
 import study.aop.order.OrderService;
-import study.aop.order.aop.AspectV1;
-import study.aop.order.aop.AspectV2;
-import study.aop.order.aop.AspectV3;
-import study.aop.order.aop.AspectV4Pointcut;
+import study.aop.order.aop.*;
 
 @Slf4j
 @SpringBootTest
 //@Import({AspectV1.class})
 //@Import({AspectV2.class})
 //@Import({AspectV3.class})
-@Import({AspectV4Pointcut.class})
+//@Import({AspectV4Pointcut.class})
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 public class AopTest {
 
     @Autowired
